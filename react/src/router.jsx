7237924f2f50be79e2dc639dc0503440aec2c9e2
Patricia <kovach.patri@gmail.com>
2024-02-22 17:25:42 +0100

    update router

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,6 +1,8 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import DefaultLayout from "./components/DefaultLayout";
 import NotFound from "./views/NotFound";
+import Projects from "./views/Projects";
+import ProjectForm from "./views/ProjectForm";
 
 const router = createBrowserRouter([
     {
@@ -8,9 +10,9 @@ const router = createBrowserRouter([
         element: <DefaultLayout />,
         children: [
             { path: "/", element: <Navigate to="/projects" /> },
-            { path: "/projects" },
-            { path: "/projects/new" },
-            { path: "/projects/:id" },
+            { path: "/projects", element: <Projects /> },
+            { path: "/projects/new", element: <ProjectForm key="create" /> },
+            { path: "/projects/:id", element: <ProjectForm key="update" /> },
         ],
     },
     {
